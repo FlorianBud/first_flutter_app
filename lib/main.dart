ea2e88f339053f4ee3efe392ac8bd4cd51c5da63
flori <florianbudnik@gmail.com>
2020-12-18 19:02:09 +0100

    Das ist ein Test

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:first_flutter_app/ui/home.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+//
 //void main() => runApp(ScaffoldExample());
 
 final ThemeData _appTheme = _buildAppTheme();
